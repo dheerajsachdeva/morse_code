906935ff854926f768dff250b65dee48bb8310b8
Dheeraj Sachdeva <dheerajarya@gmail.com>
2023-05-10 20:24:56 +0530

    Update linter errors and readme file

--- morse_code.rb
+++ morse_code.rb
@@ -31,26 +31,28 @@ def decode_char(char)
   MORSE_CODE[char]
 end
 
-puts decode_char(".-")
+puts decode_char('.-')
 
- def decode_word(word)
-    decode=""
-    result = word.split
-        result.each do |char| decode += decode_char(char)
-        end
-        decode
-    end
-
-    puts decode_word("-- -.--")
-
-def entire_message(message)
-    decode = ""
-    result = message.split('  ')
-    result.each do |word| decode += "#{decode_word(word)} "
-    end
-    decode
+def decode_word(word)
+  decode = ''
+  result = word.split
+  result.each do |char|
+    decode += decode_char(char)
+  end
+  decode
 end
 
-puts entire_message("-- -.--   -. .- -- .")
+puts decode_word('-- -.--')
 
-puts entire_message(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+def entire_message(message)
+  decode = ''
+  result = message.split('  ')
+  result.each do |word|
+    decode += "#{decode_word(word)} "
+  end
+  decode
+end
+
+puts entire_message('-- -.--   -. .- -- .')
+
+puts entire_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
